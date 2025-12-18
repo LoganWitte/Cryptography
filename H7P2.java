7242df5c2fcee942b65ffdc66a8e13905d6ddf22
LoganWitte <156727477+LoganWitte@users.noreply.github.com>
2025-12-18 15:37:36 -0500

    Final commit

---
+++ H7P2.java
@@ -0,0 +1,11 @@
+// Logan Witte
+// 11/28/25
+// For homework 7 problem 2
+
+public class H7P2 {
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------------------");
+        System.out.println("H7P2");
+        System.out.println("--------------------------------------------------");
+    }
+}
